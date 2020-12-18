ea148e676dbd17ed65da6a7c6ad062510fb697e6
Farha <anjumsyed59@gmail.com>
2020-12-18 16:20:39 +0530

    commit1

---
+++ demo1/src/com/demo1/Calc.java
@@ -0,0 +1,11 @@
+package com.demo1;
+
+public class Calc {
+	public void add(int num1,int num2) {
+		System.out.println("Sum= "+(num1+num2));
+	}
+	public void subs(int num1,int num2) {
+		System.out.println("Sum= "+(num1-num2));
+	}
+
+}
